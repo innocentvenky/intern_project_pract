321e227352d44d205077061b8192d50f9c01990c
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-06-18 18:18:12 +0530

    views.py updated

--- project1/userapp/views.py
+++ project1/userapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from userapp.models import Register
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        date_of_birth=request.POST.get('date_of_birthy')
+        phone_number=request.POST.get('phone_number')
+        state=request.POST.get('state')
+        city=request.POST.get('city')
+        gender=request.POST.get('gender')
+        pincode=request.POST.get('pincode')
+        Register.objects.create(first_name=first_name, last_name=last_name,email=email,password=password,date_of_birth=date_of_birth,phone_number=phone_number,state=state,city=city,gender=gender,pincode=pincode)
+    return render(request,'registration.html')
