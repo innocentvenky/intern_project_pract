2f738ef7cc57a14f1c57338b8896402bb6e6562b
palle-pujithareddy <pallepallepujithareddy@gmail.com>
2026-06-18 11:10:48 +0530

    updated the task and files

---
+++ userapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Register(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    frist_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length= 20)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=15)
+    state = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    gender_choices = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other')
+    ]
+    gender = models.CharField(max_length=10, choices=gender_choices)
+    pincode = models.IntegerField()
+    def __str__(self):
+        return self.name
