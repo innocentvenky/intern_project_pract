52867f8d343a965d718b607453ed5bbde885bda4
unknown <chaitanyasai2008@gmail.com>
2026-06-18 18:09:15 +0530

    updated views.py

--- project1/userapp/views.py
+++ project1/userapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from userapp.models import Register
 # Create your views here.
+def login(request):
+    if request.method=='POST':
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        user = Register.objects.get(
+            email=email,
+            password=password
+        )
+        if user:
+            return redirect('/home/')
+        else:
+            return render(request,'login.html',{'error':'Invalid email or password'})
+    return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html')
+    
