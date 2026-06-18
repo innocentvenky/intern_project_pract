15f794b3b58653709c9c0d7b95d061b329e216d4
alakambhavya <alakambhavya@gmail.com>
2026-06-18 19:00:50 +0530

    urls.py updated

--- project1/project1/urls.py
+++ project1/project1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from userapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',views.login)
 ]
